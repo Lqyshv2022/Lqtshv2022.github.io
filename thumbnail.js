9c2f3c96dd226ad47f49d1e4a02b05b17774e0fa
Lqyshv2022 <101320265+Lqyshv2022@users.noreply.github.com>
2022-03-16 00:09:37 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if (targetImage.classList.contains("small")) {
         targetImage.addEventListener("click", function() {
             targetImage.classList.remove("small");
-		};
+		});
     } else {
         targetImage.addEventListener("click", function() {
             targetImage.classList.add("small");
-		};
-    };
+		});
+    }
 };
