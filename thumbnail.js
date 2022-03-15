c6038f6468f997e95e77e1ee76ada9eb591f13a5
Lqyshv2022 <101320265+Lqyshv2022@users.noreply.github.com>
2022-03-16 01:03:45 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,15 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	let targetImage = document.querySelector("#smart-image");
-    
-    if (targetImage.classList.contains("small")) {
-        targetImage.addEventListener("click", function() {
-            alert("点击了图片");
-            targetImage.classList.remove("small");
-        });
-    } else {
-        targetImage.addEventListener("click", function() {
-            alert("点击了图片");
-            targetImage.classList.add("small");
-        });
-    };
+
+    targetImage.addEventListener("click", function() {
+        alert("点击了图片");
+        targetImage.classList.remove("small");
+    });
+
 });
