793fca2e7a7b048e8609efe77b69b6ea2e1f5098
Lqyshv2022 <101320265+Lqyshv2022@users.noreply.github.com>
2022-03-16 00:39:07 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    alert("Hello JavaScript");
+	let targetImage = document.querySelector("#smart-image");
+    
+    if (targetImage.classList.contains("small")) {
+        targetImage.addEventListener("click", function() {
+            alert("点击了图片");
+            targetImage.classList.remove("small");
+        });
+    } else {
+        targetImage.addEventListener("click", function() {
+            alert("点击了图片");
+            targetImage.classList.add("small");
+        });
+    };
 });
